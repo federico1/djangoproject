996dba5d0e2aa86e195e6f5f51dac079669962bf
federico1 <federico.roca@yahoo.com>
2019-09-23 09:30:38 -0400

    Update settings.py

--- djangoproject/settings.py
+++ djangoproject/settings.py
@@ -33,12 +33,8 @@ DEBUG = True
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'newproject1',
-        'USER': 'newprojectuser1',
-        'PASSWORD': 'password',
-        'HOST': 'localhost',
-        #'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
