239bc6b65c55f5b766ccfb7afc1b0327deffe70e
Shoaib Ijaz <shoaib.ijaz8@gmail.com>
2021-02-01 22:45:25 +0500

    about

--- app_public/urls.py
+++ app_public/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
-from .views import *
+from .views import LegalView, AboutView
+
 
 urlpatterns = [
     path('legal/',
          LegalView.as_view(),
          name='legal_detail'),
+    path('about/',
+         AboutView.as_view(),
+         name='about_detail'),
 ]
--- app_public/views.py
+++ app_public/views.py
@@ -2,3 +2,7 @@ from django.views import generic
 
 class LegalView(generic.TemplateView):
     template_name = "legal/legal.html"
+
+
+class AboutView(generic.TemplateView):
+    template_name = "about/about.html"
