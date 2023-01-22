73ea873ae532d6ebd3635236eeb7e6873597309d
Shoaib Ijaz <shoaib.ijaz8@gmail.com>
2023-01-23 01:09:50 +0500

    added cache_page to index

--- djangoproject/urls.py
+++ djangoproject/urls.py
@@ -25,6 +25,7 @@ from django.conf.urls import (handler400, handler403, handler404, handler500)
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+from django.views.decorators.cache import cache_page
 
 from .sitemap import StaticViewSitemap, SubjectsSitemap, CourseSitemap
 
@@ -61,7 +62,7 @@ urlpatterns = [
 
     path('admin/', admin.site.urls),
     path('course/', include('courses.urls')),
-    path('courses', CourseListView.as_view(), name='courses_list'),
+    path('courses', cache_page(60 * 15)(CourseListView.as_view()), name='courses_list'),
     path('', IndexView.as_view(), name='course_list'),
     path('teachers/', include('app_teachers.urls')),
     path('quiz/', include('app_quiz.urls')),
