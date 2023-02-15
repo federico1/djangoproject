f13bf515f3281cacfe59b24a352c86cdc8a799d1
Shoaib Ijaz <shoaib.ijaz8@gmail.com>
2023-02-15 15:15:14 +0500

    order confirm mail

--- app_cart/urls.py
+++ app_cart/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import CartView, CheckoutView, OrderView, PackageCartView
+from .views import CartView, CheckoutView, OrderView, PackageCartView, SendOrderConfirmMail
 
 urlpatterns = [
     path('cart/',
@@ -14,4 +14,7 @@ urlpatterns = [
     path('package-buy/<slug:slug>/',
          PackageCartView.as_view(),
          name='package_cart'),
+    path('send-order-confirm-mail',
+         SendOrderConfirmMail,
+         name='send_order_confirm_mail'),
 ]
--- app_cart/views.py
+++ app_cart/views.py
@@ -1,7 +1,10 @@
 from django.views import generic
+from django.http import HttpResponse
 from django.utils.decorators import method_decorator
 from compression_middleware.decorators import compress_page
 
+from django.template.loader import get_template
+from django.core.mail import send_mail
 
 @method_decorator(compress_page, name="dispatch")
 class CartView(generic.TemplateView):
@@ -32,3 +35,28 @@ class PackageCartView(generic.TemplateView):
         context['id'] = self.kwargs['slug']
         return context
 
+
+def SendOrderConfirmMail(request):
+  
+    ref = ''
+    result = 0
+
+    if 'ref' in request.POST and request.POST['ref'].strip():
+        ref = request.POST['ref']
+
+    if ref:
+        ctx = {ref:ref}
+        title = "You're in! Here's your order confirmation."
+        html_message = get_template(
+        "_mail_order_confirm.html").render(ctx)
+        send_mail(
+        subject=title,
+        message=" Thank you for your purchase! This email is to confirm your order with pdhsafety.com",
+        html_message=html_message,
+        from_email='mail@pdhsafety.com',
+        recipient_list=[request.user.email],
+        fail_silently=False)
+        result = 1
+
+
+    return HttpResponse(result)
