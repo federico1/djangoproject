d82d64323a9d62a5b79db0419f34a40b49e0ff0e
Shoaib Ijaz <shoaib.ijaz8@gmail.com>
2024-10-25 03:09:51 +0500

    fix bs

--- app_business/urls.py
+++ app_business/urls.py
@@ -8,42 +8,42 @@ urlpatterns = [
          dashboard_views.HomeView.as_view(),
          name='business_dashboard'),
     path('employees/',
-         never_cache(cache_page(10)
+         never_cache(cache_page(60*60)
                      (employee_views.EmployeesTemplateView.as_view())),
          name='business_employees'),
     path('employees-manage/',
-         never_cache(cache_page(10)
+         never_cache(cache_page(60*60)
                      (employee_views.EmployeesManageView.as_view())),
          name='business_employees_manage'),
     path('employees/manage/<id>/',
-         never_cache(cache_page(10)
+         never_cache(cache_page(60*60)
                      (employee_views.SingleEmployeeManageView.as_view())),
          name='business_employee_detail'),
     path('buy-courses/',
-         never_cache(cache_page(10)
+         never_cache(cache_page(60*60)
                      (course_views.BuyCoursesTemplateView.as_view())),
          name='business_buy_courses'),
     path('my-cart/',
-         never_cache(cache_page(10)
+         never_cache(cache_page(60*60)
                      (course_views.MyCartTemplateView.as_view())),
          name='business_my_cart'),
     path('my-checkout/',
-         never_cache(cache_page(10)
+         never_cache(cache_page(60*60)
                      (course_views.CheckoutTemplateView.as_view())),
          name='business_my_checkout'),
     path('my-checkout-post-order/',
-         never_cache(cache_page(10)
+         never_cache(cache_page(60*60)
                      (course_views.CheckoutPostOrderView.as_view())),
          name='business_my_checkout_post_order'),
     path('purchased-courses/',
-         never_cache(cache_page(10)
+         never_cache(cache_page(60*60)
                      (course_views.MyCoursesTemplateView.as_view())),
          name='business_my_courses'),
     path('purchased-courses-manage/',
-         never_cache(cache_page(10)
+         never_cache(cache_page(60*60)
                      (course_views.MyCoursesManageView.as_view())),
          name='business_my_courses_manage'),
-    path('account-register/', never_cache(cache_page(10)
+    path('account-register/', never_cache(cache_page(60*60)
                                           (public_views.RegisterBusinessUserView.as_view())),
          name='business_user_register'),
 ]
